0029253a7951ec8e375f2d90b3c9eb11ee647cfc
RHYTHMMIRDHA <rhythmmirdha2005@gmail.com>
2026-07-09 23:30:58 +0530

    Contact Page Added

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext';
 import {assets} from '../assets/assets'
+import RelatedDoctors from '../components/RelatedDoctors';
 
 const Appointment = () => {
 
@@ -136,6 +137,11 @@ const Appointment = () => {
         </button>
       </div>
 
+
+        {/* LISTING RELATED DOCTORS */}
+        <RelatedDoctors docId={docId} speciality = {docInfo.speciality}>
+
+        </RelatedDoctors>
     </div>
   );
 }
