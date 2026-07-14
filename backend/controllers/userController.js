a69e6ad08f7f485ccdba631a33683c99d919cf0c
RHYTHMMIRDHA <rhythmmirdha2005@gmail.com>
2026-07-14 19:01:20 +0530

    Update User Controller Added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ import validator from 'validator'
 import bcrypt from 'bcrypt'
 import userModel from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
+import {v2 as cloudinary} from 'cloudinary'
 
 // api to register user
 
@@ -77,4 +78,57 @@ const loginUser = async (req, res) => {
     }
 }
 
-export {registerUser, loginUser}
+// api to get user profile data
+// to get the userId -> we will use Token -> need a middleware -> authUser
+const getProfile = async(req, res) => {
+    try {
+        
+        const { userId } = req.body
+        const userData = await userModel.findById(userId).select('-password')
+
+        res.json({success:true, userData})
+
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
+}
+
+// api to Update UserProfile
+
+const updateProfile = async (req, res) => {
+    try {
+
+        const {userId, name, phone, address, dob, gender} = req.body
+        const imageFile = req.file
+
+        if(!name || !phone || !dob || !gender){
+            return res.json({success:false, message:'Data is missing'})
+        }
+
+        await userModel.findByIdAndDelete(userId, {name, phone, address:JSON.parse(address), dob, gender})
+
+        if(imageFile){
+
+            // upload image to cloudinary
+            const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type:'image'})
+
+            const imageUrl = imageUpload.secure_url
+
+            // save to db
+            await userModel.findByIdAndUpdate(userId, {image:imageUrl})
+        }
+
+        res.json({
+            success: true,
+            message: 'Profile Updated'
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
+}
+
+export {registerUser, loginUser, getProfile, updateProfile}
